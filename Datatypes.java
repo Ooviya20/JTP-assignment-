57f06d4febd00455b6130faaac8a8d8f55292081
Ooviya20 <89766291+Ooviya20@users.noreply.github.com>
2021-08-30 12:51:06 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes {
+    public static void main(String args[]) {
+      int a=10;
+      float b=25.67867f;
+      double c=893;
+      char d='T';
+      String e="Java" ;
+
+      System.out.println("int is "+a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(e);
+    }
+}
